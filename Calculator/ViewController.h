8a3109e824da8d7059af39a2ed03d01e0b6772da
wiseguy16 <gergusa04@gmail.com>
2016-07-21 12:10:32 -0400

    Initial Commit

---
+++ Calculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Calculator
+//
+//  Created by Gregory Weiss on 7/21/16.
+//  Copyright © 2016 Gregory Weiss. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
